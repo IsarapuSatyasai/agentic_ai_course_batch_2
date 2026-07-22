7042ac13d6818c15b502429599b74d104bb63b6a
= <=>
2026-07-22 21:28:41 +0530

    Minor modifications done on 12th and 13th session's files

--- week_3/Day_13/demo/config.py
+++ week_3/Day_13/demo/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-OPENAI_API_KEY  = os.getenv("OPENAI_API_KEY")
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 if not OPENAI_API_KEY:
-    raise ValueError("OPENAI_API_KEY is not set in the environment variables.")
+    raise ValueError('OPENAI_API_KEY is not set in .env file')
