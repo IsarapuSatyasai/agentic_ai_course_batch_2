3d16f02cdf6e770bdc889e9ab5e3555b9f25099b
= <=>
2026-07-28 19:40:40 +0530

    Added 17th session files.

--- week_4/Day_16/vector-embedding-demo/embed_demo.py
+++ week_4/Day_16/vector-embedding-demo/embed_demo.py
@@ -9,7 +9,7 @@ client = OpenAI()
 def get_embedding(text):
     response = client.embeddings.create(
         input=text,
-        model="text-embedding-3-small"
+        model="text-embedding-3-small",
     )
     return response.data[0].embedding
 
@@ -17,24 +17,24 @@ def similarity(a, b):
     return cosine_similarity([a], [b])[0][0]
 
 # 1. What is a Vector Embedding?
-print("=== 1. Vector Embedding ===")
+print("1. What is a Vector Embedding?")
 cat = get_embedding("cat")
-print("Length:", len(cat))
-print("First 8 numbers:", [round(x, 4) for x in cat[:8]])
+print("Length :", len(cat))
+print("First 8 numbers: ", [round(x, 4) for x in cat[:8]])
 
 # 2. Similar things live close
-print("\n=== 2. Similarity ===")
+print("2. Similarity")
 dog = get_embedding("dog")
 car = get_embedding("car")
-print("cat ↔ dog :", round(similarity(cat, dog), 4))
-print("cat ↔ car :", round(similarity(cat, car), 4))
 
-# 3. Famous math: King - Man + Woman ≈ Queen
-print("\n=== 3. King - Man + Woman ≈ Queen ===")
-king  = np.array(get_embedding("king"))
-man   = np.array(get_embedding("man"))
+print("cat <-> dog :", round(similarity(cat, dog), 4))
+print("cat <-> car :", round(similarity(cat, car), 4))
+
+# 3. Famouse math: King - Man + Woman = Queen
+print("Famous math: King - Man + Woman = Queen")
+king = np.array(get_embedding("king"))
+man = np.array(get_embedding("man"))
 woman = np.array(get_embedding("woman"))
-queen = np.array(get_embedding("queen"))
+queen = king - man + woman
 
-result = king - man + woman
-print("Result ↔ queen :", round(similarity(result, queen), 4))
+print("king <-> queen :", round(similarity(king, queen), 4))
