f744416bdd77bd5e9583924abbc859d178305a58
IsarapuSatyasai <esarapusathyasai603@gmail.com>
2026-08-13 19:03:53 +0530

    minor updates added.

--- week_6/Day_26/ReAct/agents/react_agent.py
+++ week_6/Day_26/ReAct/agents/react_agent.py
@@ -9,31 +9,34 @@ tools = [calculate, search_web, explore_dead_letters]
 
 prompt = PromptTemplate.from_template(
     """Answer the following questions as best you can. You have access to the following tools:
-
-{tools}
-
-Use the following format:
-
-Question: the input question you must answer
-Thought: you should always think about what to do
-Action: the action to take, should be one of [{tool_names}]
-Action Input: the input to the action
-Observation: the result of the action
-... (this Thought/Action/Action Input/Observation can repeat N times)
-Thought: I now know the final answer
-Final Answer: the final answer to the original input question
-
-Begin!
-
-Question: {input}
-Thought:{agent_scratchpad}"""
+    
+    {tools}
+    
+    User the following format:
+    
+    Question: the input question you must answer
+    Thought: you should always think about what to do
+    Action: the action to take, should be one of [{tool_names}]
+    Action Input: the input to the action
+    Observation: the result of the action
+    ... (this Thought/Action/Action Input/Observation: the result can repeat N times)
+    Thought: I now know the final answer
+    Final Answer: the final answer to the original input question
+    
+    Begin!
+    Question: {input}
+    Thought:{agent_scratchpad}"""
 )
 
-agent = create_react_agent(llm=llm, tools=tools, prompt=prompt)
+agent = create_react_agent(
+    llm=llm,
+    tools=tools,
+    prompt=prompt
+)
 
 agent_executor = AgentExecutor(
-    agent=agent, 
-    tools=tools, 
+    agent=agent,
+    tools=tools,
     verbose=True,
     max_iterations=10,
     handle_parsing_errors=True
